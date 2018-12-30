6d8b34b843737f582890802f1d70047f8d09acb9
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2018-12-30 11:33:34 -0200

    Header Component

---
+++ components/layouts/default/header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <default-nav/>
+        <default-slider/>
+    </header>
+</template>
+
+<script>
+  import defaultNav from '@/components/layouts/default/nav'
+  import defaultSlider from '@/components/layouts/default/slider'
+
+  export default {
+    name: "Layout",
+    components: {defaultNav,defaultSlider},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
