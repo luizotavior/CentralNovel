de91e4a713ae931faf5484dcd6a318c145d7c35a
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2018-12-30 01:57:56 -0200

    Atualização Layout Default v.3

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,36 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation">
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/">
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28">
-        </a>
-
-        <div class="navbar-burger">
-          <span/>
-          <span/>
-          <span/>
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key">
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active">
-              <b-icon :icon="item.icon"/> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-
-    </section>
+  <div id="layout-default">
+    <default-header/>
+    <nuxt/>
+    <default-legado/>
+    <default-footer/>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      items: [
-        { title: 'Home', icon: 'home', to: { name: 'index' } },
-        { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } }
-      ]
+  import defaultHeader from '@/components/layouts/default/header'
+
+  export default {
+    name: "Layout Default",
+    components: {
+      defaultHeader
+    },
+    data() {
+      return {
+      }
     }
   }
-}
 </script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700');
+  #layout-default {
+    display: flex;
+    flex-direction: column;
+    background-image: url('/images/bg/nuvens.jpg');
+    background-repeat: repeat-y;
+    background-size: 100%; 
+    font-family: 'Source Sans Pro', sans-serif;
+	  font-weight: 400;
+  }
+</style>
