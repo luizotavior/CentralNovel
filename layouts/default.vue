5bbe32d7638f274706b5cbcefbbf8d8d9c18182a
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2019-01-01 02:28:20 -0200

    Adicionando Font Italic

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,7 +35,7 @@
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700');
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,700');
   #layout-default {
     display: flex;
     flex-direction: column;
