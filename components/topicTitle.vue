a69f060f66bb78f96b223718f848d8caff7510f7
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2019-01-01 23:49:03 -0200

    Componente Topic Title

---
+++ components/topicTitle.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="topic-title">
+        <h2>{{ title }}</h2>
+        <a class="see-more" v-if="seeMore != null" :href="seeMore" title="Veja Mais">Veja Mais</a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        seeMore: {
+            type: String,
+            default: null
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "@/assets/sass/layout/_grid.scss";
+    @import '@/assets/sass/helpers/_variables.scss';
+    div.topic-title{
+        margin-bottom: 22px;
+        display: flex;
+        flex-direction: row;
+        padding: 0 12px;
+        @extend %justify-between;
+        @extend %vertical-align-middle;
+        h2{
+            font-size: 32px; 
+        }
+        a.see-more{
+            min-width: 62px;
+        }
+    }
+</style>
+
