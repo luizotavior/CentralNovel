b20f7f5cca4ddbd612eafae48aa8f2ff5791e05e
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2019-01-05 23:35:59 -0200

    Correção Slider na Index

--- components/layouts/default/header.vue
+++ components/layouts/default/header.vue
@@ -1,17 +1,15 @@
 <template>
     <header>
         <default-nav/>
-        <default-slider/>
     </header>
 </template>
 
 <script>
   import defaultNav from '@/components/layouts/default/nav'
-  import defaultSlider from '@/components/layouts/default/slider'
 
   export default {
     name: "Layout",
-    components: {defaultNav,defaultSlider},
+    components: {defaultNav},
 }
 </script>
 
