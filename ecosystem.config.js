90393f472897182b17a92bf9aa7805061ad71b02
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2019-08-10 20:31:42 -0300

    PM2 Config

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+      name      : 'Central Novel', // App name that shows in `pm2 ls`
+      exec_mode : 'cluster', // enables clustering
+      instances : 'max', // or an integer
+      //cwd       : './current', // only if using a subdirectory
+      script    : './node_modules/nuxt/bin/nuxt-start', // The magic key
+  }]
+};
