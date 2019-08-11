8fd9a751bcc772688193d2dadf5f284ac5c19a3a
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2019-08-10 21:07:37 -0300

    PM2 Configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,6 @@ module.exports = {
       exec_mode : 'cluster', // enables clustering
       instances : 'max', // or an integer
       //cwd       : './current', // only if using a subdirectory
-      script    : './node_modules/nuxt/bin/nuxt-start', // The magic key
+      script    : './node_modules/nuxt-start/bin/nuxt-start.js', // The magic key
   }]
 };
