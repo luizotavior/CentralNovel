947069c899144658ade4b0ddac38e046277dbac0
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2019-08-11 01:05:17 -0300

    Revert

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps : [{
-      name      : 'Central Novel', // App name that shows in `pm2 ls`
-      exec_mode : 'cluster', // enables clustering
-      instances : 'max', // or an integer
-      //cwd       : './current', // only if using a subdirectory
-      script    : './node_modules/nuxt-start/bin/nuxt-start.js', // The magic key
-  }]
-};
