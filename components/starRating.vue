639b6f08dc95a1d38615c8d74dd80cb68bba726e
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2020-08-13 22:02:48 -0300

    Adicionando novas Props para personalização do Componentes

--- components/starRating.vue
+++ components/starRating.vue
@@ -1,50 +1,76 @@
 <template>
-    <div class="star-rating">
-        <div class="stars">
-            <b-icon v-for="n in maxRating" :key="n" size="is-small" :icon="(formattedRating >= n ? 'star' : (Math.ceil(formattedRating) == n ? 'star-half' : 'star-outline' ))"/>
-        </div>
-        <span class="rating">{{ formattedRating }}</span>
+  <div class="star-rating">
+    <div class="stars">
+      <b-icon
+        v-for="n in maxRating"
+        :key="n"
+        :size="size"
+        :icon="(formattedRating >= n ? 'star' : (Math.ceil(formattedRating) == n ? 'star-half' : 'star-outline' ))"
+      />
     </div>
+    <div class="__texts">
+      <span :class="size">{{ formattedRating }}</span>
+      <span
+        :class="size"
+        v-if="currentVotes != null"
+      >({{currentVotes}} Avaliações)</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'BTableColumn',
-    props: {
-        currentRating: {
-            type: Number,
-            default: 0
-        },
-        maxRating: {
-            type: Number,
-            default: 5
-        },
+  name: 'BTableColumn',
+  props: {
+
+    size: {
+      type: String,
+      default: "is-small"
     },
-    computed: {
-        formattedRating() {
-            return (parseFloat(this.currentRating * 100) / 100).toFixed(1);
-        },
+    currentVotes: {
+      type: Number,
+      default: null
     },
+    currentRating: {
+      type: Number,
+      default: 0
+    },
+    maxRating: {
+      type: Number,
+      default: 5
+    },
+  },
+  computed: {
+    formattedRating () {
+      return (parseFloat(this.currentRating * 100) / 100).toFixed(1);
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-    @import "@/assets/sass/layout/_grid.scss";
-    @import '@/assets/sass/helpers/_variables.scss';
-    .star-rating{
-        display: flex;
-        flex-direction: row;
-        flex-wrap: nowrap;
-        .stars{
-            display: flex;
-            flex-direction: row;
-            flex-wrap: nowrap;
-            @extend %justify-between;
-            @extend %vertical-align-middle;
-            color: #ff8d29;
-        }
-        .rating{
-            margin-left: 4px;
-        }
+@import "@/assets/sass/layout/_grid.scss";
+@import "@/assets/sass/helpers/_variables.scss";
+.star-rating {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  .stars {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    @extend %justify-between;
+    @extend %vertical-align-middle;
+    color: #ff8d29;
+  }
+  .__texts {
+    margin-left: 3px;
+    .is-small {
+      font-size: 16px;
     }
+    .is-medium {
+      font-size: 20px;
+    }
+  }
+}
 </style>
