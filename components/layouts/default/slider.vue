c9c9ec94931cd1e44f39780727680fa4778344bd
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2020-09-02 00:03:52 -0300

    Novos Slides com Link

--- components/layouts/default/slider.vue
+++ components/layouts/default/slider.vue
@@ -10,22 +10,31 @@
       >
         <div class="slide">
           <img
-            src="images/slides/slide_01.jpg"
+            src="images/slides/slide_01.png"
             alt=""
           >
         </div>
 
         <div class="slide">
-          <img
-            src="images/slides/slide_02.png"
-            alt=""
-          >
+
+          <a href="/series/against-the-gods">
+            <img
+              src="images/slides/slide_02.png"
+              alt=""
+            >
+          </a>
         </div>
         <div class="slide">
-          <img
-            src="images/slides/slide_03.png"
-            alt=""
+          <a
+            href="https://www.amazon.com.br/s?k=novel+jbc+newpop&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss"
+            target="_blank"
+            rel="noopener noreferrer"
           >
+            <img
+              src="images/slides/slide_03.png"
+              alt=""
+            >
+          </a>
         </div>
         <div class="slide">
           <img
@@ -34,10 +43,16 @@
           >
         </div>
         <div class="slide">
-          <img
-            src="images/slides/slide_05.png"
-            alt=""
+          <a
+            href="http://novels.com.br/dicas-de-leitura/as-10-novels-mais-indicadas-para-entrar-no-mundo-do-cultivo/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
+            <img
+              src="images/slides/slide_05.png"
+              alt=""
+            >
+          </a>
         </div>
       </agile>
     </no-ssr>
