dc8b87570cc95caaa17a2b8fa783915a82c0fad9
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2020-11-25 17:09:12 -0300

    Slide Manutenção

--- components/layouts/default/slider.vue
+++ components/layouts/default/slider.vue
@@ -2,7 +2,7 @@
   <main>
     <no-ssr>
       <agile
-        :autoplay="true"
+        :autoplay="false"
         :autoplaySpeed="6000"
         :arrows="false"
         :dots="false"
@@ -10,61 +10,9 @@
       >
         <div class="slide">
           <img
-            src="images/slides/slide_01.png"
+            src="images/slides/slide_00.png"
             alt=""
           >
-        </div>
-        <div class="slide">
-
-          <a
-            href="https://tsundokutraducoes.com.br/"
-            target="_blank"
-          >
-            <img
-              src="images/slides/slide_06.png"
-              alt=""
-            >
-          </a>
-        </div>
-
-        <div class="slide">
-
-          <a href="/series/against-the-gods">
-            <img
-              src="images/slides/slide_02.png"
-              alt=""
-            >
-          </a>
-        </div>
-        <div class="slide">
-          <a
-            href="https://www.amazon.com.br/s?k=novel+jbc+newpop&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="images/slides/slide_03.png"
-              alt=""
-            >
-          </a>
-        </div>
-        <div class="slide">
-          <img
-            src="images/slides/slide_04.png"
-            alt=""
-          >
-        </div>
-        <div class="slide">
-          <a
-            href="http://novels.com.br/dicas-de-leitura/as-10-novels-mais-indicadas-para-entrar-no-mundo-do-cultivo/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="images/slides/slide_05.png"
-              alt=""
-            >
-          </a>
         </div>
       </agile>
     </no-ssr>
